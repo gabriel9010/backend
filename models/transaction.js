8845c04c8ee4b5f59918d1a8e8d29eae1c03a8dc
gabriel123 <gabrielclaudio30@gmail.com>
2024-06-07 18:53:27 +0200

    todo

---
+++ models/transaction.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
